d3b76f48430a699d34849cf1efb79b68c6231ac7
Connor van Spronssen <info@dobefu.com>
2025-08-01 12:35:41 +0200

    feat: Add a method to get the binding power of a token

---
+++ internal/parser/get_binding_power.go
@@ -0,0 +1,22 @@
+package parser
+
+import (
+	"github.com/Dobefu/pratt-parser/internal/token"
+)
+
+// getBindingPower returns the binding power of the current token.
+func (p *Parser) getBindingPower(currentToken token.Token) int {
+	switch currentToken.TokenType {
+	case token.TokenTypeLParen, token.TokenTypeRParen:
+		return 1000
+
+	case token.TokenTypeOperationMul, token.TokenTypeOperationDiv:
+		return 200
+
+	case token.TokenTypeOperationAdd, token.TokenTypeOperationSub:
+		return 100
+
+	default:
+		return 0
+	}
+}
