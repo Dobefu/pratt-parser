b32a84b5604259b2ebc1815032cb6603ddc3178c
Connor van Spronssen <info@dobefu.com>
2025-08-02 15:08:45 +0200

    test: Add some tests to the Parser method

--- internal/parser/parse_test.go
+++ internal/parser/parse_test.go
@@ -1,6 +1,54 @@
 package parser
 
-import "testing"
+import (
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "1",
+			expected: "1",
+		},
+		{
+			input:    "1 + 2",
+			expected: "(1 + 2)",
+		},
+		{
+			input:    "1 + 2 * 3",
+			expected: "(1 + (2 * 3))",
+		},
+		{
+			input:    "1 + 2 * 3 / 4",
+			expected: "(1 + ((2 * 3) / 4))",
+		},
+		{
+			input:    "1 + 2 * 3 / 4 - 5",
+			expected: "((1 + ((2 * 3) / 4)) - 5)",
+		},
+		{
+			input:    "(1 + 10 / 5 + 2 * 5 - ( -123 - (-(5))))",
+			expected: "(((1 + (10 / 5)) + (2 * 5)) - ((- 123) - (- 5)))",
+		},
+	}
+
+	for _, test := range tests {
+		parser := NewParser(test.input)
+		ast, err := parser.Parse()
+
+		if err != nil {
+			t.Errorf("expected no error, got %v", err)
+			continue
+		}
+
+		if ast.Expr() != test.expected {
+			t.Errorf("expected %s, got %s", test.expected, ast.Expr())
+		}
+	}
+}
 
 func BenchmarkParse(b *testing.B) {
 	for b.Loop() {
