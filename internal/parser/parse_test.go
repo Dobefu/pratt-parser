c2bc8e10da17a7e55bd276f11c0d532dd72ad909
Connor van Spronssen <info@dobefu.com>
2025-08-03 10:24:53 +0200

    test: Add some error tests to the Parse method

--- internal/parser/parse_test.go
+++ internal/parser/parse_test.go
@@ -61,6 +61,35 @@ func TestParse(t *testing.T) {
 	}
 }
 
+func TestParseErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input string
+	}{
+		{
+			input: "",
+		},
+		{
+			input: "1 +",
+		},
+		{
+			input: "1 + 2 *",
+		},
+		{
+			input: "/ 1",
+		},
+	}
+
+	for _, test := range tests {
+		_, err := NewParser(test.input).Parse()
+
+		if err == nil {
+			t.Errorf("expected error for %s, got none", test.input)
+		}
+	}
+}
+
 func BenchmarkParse(b *testing.B) {
 	for b.Loop() {
 		p := NewParser("1 + 2 * 3 / 4")
