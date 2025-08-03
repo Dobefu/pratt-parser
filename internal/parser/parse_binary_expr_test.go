4c8bf6098fc43ebed10c2bac79303e0011108ffc
Connor van Spronssen <info@dobefu.com>
2025-08-03 10:30:39 +0200

    test: Add tests to the parseBinaryExpr method

--- internal/parser/parse_binary_expr_test.go
+++ internal/parser/parse_binary_expr_test.go
@@ -6,6 +6,67 @@ import (
 	"github.com/Dobefu/pratt-parser/internal/token"
 )
 
+func TestParseBinaryExpr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{
+			input:    "1 + 1",
+			expected: "(1 + 1)",
+		},
+	}
+
+	for _, test := range tests {
+		parser := NewParser(test.input)
+		ast, err := parser.Parse()
+
+		if err != nil {
+			t.Errorf("expected no error, got %v", err)
+
+			continue
+		}
+
+		if ast.Expr() != test.expected {
+			t.Errorf("expected %s, got %s", test.expected, ast.Expr())
+		}
+	}
+}
+
+func TestParseBinaryExprErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input string
+	}{
+		{
+			input: "",
+		},
+		{
+			input: "1 +",
+		},
+		{
+			input: "1 + 2 *",
+		},
+		{
+			input: "1 + 2 * 3 /",
+		},
+		{
+			input: "1 💔 1",
+		},
+	}
+
+	for _, test := range tests {
+		_, err := NewParser(test.input).Parse()
+
+		if err == nil {
+			t.Errorf("expected error for %s, got none", test.input)
+		}
+	}
+}
+
 func BenchmarkParseBinaryExpr(b *testing.B) {
 	for b.Loop() {
 		p := NewParser("")
