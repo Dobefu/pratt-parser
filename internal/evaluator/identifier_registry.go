93b533c039b3fdf8883de0879e343812f57bc242
Connor van Spronssen <info@dobefu.com>
2025-08-04 08:13:15 +0200

    feat: Add some more mathematical constants

--- internal/evaluator/identifier_registry.go
+++ internal/evaluator/identifier_registry.go
@@ -3,9 +3,24 @@ package evaluator
 import "math"
 
 var identifierRegistry = map[string]identifierInfo{
-	"pi": {
+	"PI": {
 		handler: func() (float64, error) {
 			return math.Pi, nil
+		},
+	},
+	"TAU": {
+		handler: func() (float64, error) {
+			return math.Pi * 2, nil
+		},
+	},
+	"E": {
+		handler: func() (float64, error) {
+			return math.E, nil
+		},
+	},
+	"PHI": {
+		handler: func() (float64, error) {
+			return math.Phi, nil
 		},
 	},
 }
