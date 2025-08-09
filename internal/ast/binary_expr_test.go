1f239a212ea41edc663d630dab0703dffd882a9d
Connor van Spronssen <info@dobefu.com>
2025-08-09 11:35:25 +0200

    test: Add an additional test case for the BinaryExpr AST node

--- internal/ast/binary_expr_test.go
+++ internal/ast/binary_expr_test.go
@@ -10,8 +10,9 @@ func TestBinaryExpr(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		input    ExprNode
-		expected string
+		input       ExprNode
+		expected    string
+		expectedPos int
 	}{
 		{
 			input: &BinaryExpr{
@@ -23,13 +24,31 @@ func TestBinaryExpr(t *testing.T) {
 				},
 				Pos: 0,
 			},
-			expected: "(1 + 1)",
+			expected:    "(1 + 1)",
+			expectedPos: 0,
+		},
+		{
+			input: &BinaryExpr{
+				Left:  &NumberLiteral{Value: "1", Pos: 0},
+				Right: &NumberLiteral{Value: "2", Pos: 2},
+				Operator: token.Token{
+					Atom:      "*",
+					TokenType: token.TokenTypeOperationMul,
+				},
+				Pos: 0,
+			},
+			expected:    "(1 * 2)",
+			expectedPos: 0,
 		},
 	}
 
 	for _, test := range tests {
 		if test.input.Expr() != test.expected {
-			t.Errorf("expected %s, got %s", test.expected, test.input.Expr())
+			t.Errorf("expected '%s', got '%s'", test.expected, test.input.Expr())
+		}
+
+		if test.input.Position() != test.expectedPos {
+			t.Errorf("expected pos '%d', got '%d'", test.expectedPos, test.input.Position())
 		}
 	}
 }
