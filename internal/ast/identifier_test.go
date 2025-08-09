0ec35bc148f2afcfa765e90d4f5d7284796e638e
Connor van Spronssen <info@dobefu.com>
2025-08-09 11:31:39 +0200

    test: Add an additional test case for the Identifier AST node

--- internal/ast/identifier_test.go
+++ internal/ast/identifier_test.go
@@ -8,18 +8,29 @@ func TestIdentifier(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		input    ExprNode
-		expected string
+		input         ExprNode
+		expectedValue string
+		expectedPos   int
 	}{
 		{
-			input:    &Identifier{Value: "PI", Pos: 0},
-			expected: "PI",
+			input:         &Identifier{Value: "PI", Pos: 0},
+			expectedValue: "PI",
+			expectedPos:   0,
+		},
+		{
+			input:         &Identifier{Value: "PI", Pos: 1},
+			expectedValue: "PI",
+			expectedPos:   1,
 		},
 	}
 
 	for _, test := range tests {
-		if test.input.Expr() != test.expected {
-			t.Errorf("expected %s, got %s", test.expected, test.input.Expr())
+		if test.input.Expr() != test.expectedValue {
+			t.Errorf("expected '%s', got '%s'", test.expectedValue, test.input.Expr())
+		}
+
+		if test.input.Position() != test.expectedPos {
+			t.Errorf("expected pos '%d', got '%d'", test.expectedPos, test.input.Position())
 		}
 	}
 }
