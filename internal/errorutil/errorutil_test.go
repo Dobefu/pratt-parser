f2532cacf83299b476db62596ccdc8d04c676aa2
Connor van Spronssen <info@dobefu.com>
2025-08-09 12:42:15 +0200

    test: Add additional test cases for the Error struct

--- internal/errorutil/errorutil_test.go
+++ internal/errorutil/errorutil_test.go
@@ -2,19 +2,84 @@ package errorutil
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 )
 
-func TestErrorMsg(t *testing.T) {
+func TestNewError(t *testing.T) {
 	t.Parallel()
 
-	err := NewError(ErrorMsgParenNotClosedAtEOF)
+	tests := []struct {
+		input    ErrorMsg
+		expected string
+	}{
+		{
+			input:    ErrorMsgParenNotClosedAtEOF,
+			expected: ErrorMsgParenNotClosedAtEOF,
+		},
+	}
 
-	if errors.Unwrap(err).Error() != ErrorMsgParenNotClosedAtEOF {
-		t.Errorf(
-			"expected error to be '%s', got '%s'",
-			ErrorMsgParenNotClosedAtEOF,
-			errors.Unwrap(err).Error(),
-		)
+	for _, test := range tests {
+		err := NewError(test.input)
+
+		if err.Error() != test.expected {
+			t.Errorf(
+				"expected error to be '%s', got '%s'",
+				test.expected,
+				err.Error(),
+			)
+		}
+
+		if errors.Unwrap(err).Error() != test.expected {
+			t.Errorf(
+				"expected error to be '%s', got '%s'",
+				test.expected,
+				errors.Unwrap(err).Error(),
+			)
+		}
+	}
+}
+
+func TestNewErrorAt(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		input    ErrorMsg
+		pos      int
+		expected string
+	}{
+		{
+			input:    ErrorMsgParenNotClosedAtEOF,
+			pos:      0,
+			expected: ErrorMsgParenNotClosedAtEOF,
+		},
+	}
+
+	for _, test := range tests {
+		err := NewErrorAt(test.input, test.pos)
+
+		if err.Error() != fmt.Sprintf("%s at position %d", test.expected, test.pos) {
+			t.Errorf(
+				"expected error to be '%s', got '%s'",
+				test.expected,
+				err.Error(),
+			)
+		}
+
+		if errors.Unwrap(err).Error() != test.expected {
+			t.Errorf(
+				"expected error to be '%s', got '%s'",
+				test.expected,
+				errors.Unwrap(err).Error(),
+			)
+		}
+
+		if err.Position() != test.pos {
+			t.Errorf(
+				"expected position to be %d, got %d",
+				test.pos,
+				err.Position(),
+			)
+		}
 	}
 }
