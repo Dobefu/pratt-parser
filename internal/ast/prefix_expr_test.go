dd85ad0a1e1296229b6e8cd62ec50ac71d6c0158
Connor van Spronssen <info@dobefu.com>
2025-08-09 11:37:46 +0200

    test: Add an additional test case for the PrefixExpr AST node

--- internal/ast/prefix_expr_test.go
+++ internal/ast/prefix_expr_test.go
@@ -10,8 +10,9 @@ func TestPrefixExpr(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
-		input    ExprNode
-		expected string
+		input         ExprNode
+		expectedValue string
+		expectedPos   int
 	}{
 		{
 			input: &PrefixExpr{
@@ -30,13 +31,18 @@ func TestPrefixExpr(t *testing.T) {
 				},
 				Pos: 0,
 			},
-			expected: "(+ (1 + 1))",
+			expectedValue: "(+ (1 + 1))",
+			expectedPos:   0,
 		},
 	}
 
 	for _, test := range tests {
-		if test.input.Expr() != test.expected {
-			t.Errorf("expected %s, got %s", test.expected, test.input.Expr())
+		if test.input.Expr() != test.expectedValue {
+			t.Errorf("expected '%s', got '%s'", test.expectedValue, test.input.Expr())
+		}
+
+		if test.input.Position() != test.expectedPos {
+			t.Errorf("expected pos '%d', got '%d'", test.expectedPos, test.input.Position())
 		}
 	}
 }
